aacb1f9186c604c8695ea3e56eb12bbc6fc0db68
GYANNENDRA KUMAR <gyannendra.kumar@pathinfotech.com>
2020-11-06 10:53:50 +0530

    changed

--- src/main/java/com/ghf/presigned/service/GeneratePresignedUrlService.java
+++ src/main/java/com/ghf/presigned/service/GeneratePresignedUrlService.java
@@ -2,6 +2,8 @@ package com.ghf.presigned.service;
 
 import com.ghf.presigned.model.FileUploadedAsAnObjectViaPresignedUrl;
 
+import java.net.URL;
+
 public interface GeneratePresignedUrlService {
     public String generatePresignedUrl(FileUploadedAsAnObjectViaPresignedUrl contentUploadedAsAnObjectViaPresignedUrl);
 }
--- src/main/java/com/ghf/presigned/service/impl/GeneratePresignedUrlServiceImpl.java
+++ src/main/java/com/ghf/presigned/service/impl/GeneratePresignedUrlServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ghf.presigned.service.impl;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -8,10 +10,16 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
 import com.ghf.presigned.model.FileUploadedAsAnObjectViaPresignedUrl;
 import com.ghf.presigned.service.GeneratePresignedUrlService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.net.URL;
 import java.util.Date;
 
 
@@ -31,22 +39,19 @@ public class GeneratePresignedUrlServiceImpl implements GeneratePresignedUrlServ
     @Override
     public String generatePresignedUrl(FileUploadedAsAnObjectViaPresignedUrl fileUploadedAsAnObjectViaPresignedUrl) {
         AWSCredentials credentials = new BasicAWSCredentials(AWS_ACCESS_KEY,AWS_SECRET_KEY);
-        final AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.DEFAULT_REGION).build();
+        final AmazonS3 s3Client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(CLIENT_REGION).build();
 
         String ec2Path = fileUploadedAsAnObjectViaPresignedUrl.getEc2Path();
         String fileName = fileUploadedAsAnObjectViaPresignedUrl.getFileName();
 
-        uploadGeneratePresignedUrlRequest(AWS_BUCKET_NAME, fileName);
-        String url = getSignedUrlForS3File(s3Client, AWS_BUCKET_NAME, fileName);
-        System.out.println("Received response : " + url);
-        return url;
-    }
-
-    public GeneratePresignedUrlRequest uploadGeneratePresignedUrlRequest(String tempBucketName,String objectKey){
-        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(tempBucketName, objectKey);
-        generatePresignedUrlRequest.setMethod(HttpMethod.PUT);
-        generatePresignedUrlRequest.setContentType("application/octet-stream");
-        return generatePresignedUrlRequest;
+        try {
+            uploadObject(s3Client, AWS_BUCKET_NAME, ec2Path, ec2Path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String url = getSignedUrlForS3File(s3Client, AWS_BUCKET_NAME, ec2Path);
+        System.out.println("Received response:"+url);
+        return url.toString();
     }
 
     public static String getSignedUrlForS3File(AmazonS3 s3Client, String s3BucketName, String fileName){
@@ -56,6 +61,14 @@ public class GeneratePresignedUrlServiceImpl implements GeneratePresignedUrlServ
         String url = s3Client.generatePresignedUrl( request ).toString();
         System.out.println("Pre-Signed URL = " + url);
         return url;
+    }
+
+    public static void uploadObject(AmazonS3 s3Client, String s3BucketName, String folderPathFileName, String fileName) throws AmazonServiceException, AmazonClientException, InterruptedException{
+        TransferManager tm = new TransferManager(s3Client);
+        PutObjectRequest putObjectRequest = new PutObjectRequest(s3BucketName, folderPathFileName, new File(fileName));
+        Upload myUpload = tm.upload( putObjectRequest );
+        myUpload.waitForCompletion();
+        tm.shutdownNow();
     }
 
     public static Date getExpirationTime(){
